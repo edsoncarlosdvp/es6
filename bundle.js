bf709f9cdd9187e103742616b7cb79b99fccbb62
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2020-10-02 00:58:01 -0300

    Conceitos concluído! Iniciando Weback

--- bundle.js
+++ bundle.js
@@ -1,78 +1,170 @@
-"use strict";
+// SINTAXE CURTA DE OBJETO
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+/*
+const nome = "Edson"
+const idade = 38
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+const usuario = {
+    nome,
+    idade,
+    empresa: 'ENI'
+}
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+console.log(usuario)*/
+// TEMPLATE LITTERS
 
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+/*
+const nome = "Edson"
+const idade = 38
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+console.log(`Meu nome é ${nome} e tenho ${idade}`)*/
+// SPREAD
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+/*
+const usuario1 = {
+    nome: 'Edson',
+    idade: 38,
+    empresa: 'ENI'
+}
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+const usuario2 = { ...usuario1, nome: 'Carlos' }
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+console.log(usuario2)
+*/
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/*
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+const arr3 = [...arr1, ...arr2]
+*/
+// REST
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+/*
+function soma(a, b, ...params) {
+    return params
+}*/
 
-// COM HERANÇA
-var List = /*#__PURE__*/function () {
-  function List() {
-    _classCallCheck(this, List);
+/*
+function soma(...params) {
+    return params.reduce((total, next) => total + next)
+}*/
+//console.log(arr3)
 
-    this.data = [];
-  }
+/*
+const arr = [1, 2, 3, 4]
 
-  _createClass(List, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log(this.data);
+const [a, ...c] = arr
+*/
+
+/*
+const usuario = {
+    nome: 'Edson',
+    idade: 38,
+    empresa: 'ENI'
+}
+
+const { nome, ...resto } = usuario
+*/
+
+/* DESESTRUTURAÇÃO */
+
+/*const usuario = {
+    nome: 'Edson',
+    idade: 38,
+    endereco: {
+        cidade: 'Nova Iguaçu',
+        estado: 'RJ',
     }
-  }]);
+}*/
+//const { nome, idade, endereco: { cidade, estado } } = usuario
+// TAMBÉM EM FUNÇÃO
+//function mostraNome({ nome, idade }) {
+//console.log(nome, idade)
+//}
+//mostraNome(usuario)
+//console.log(nome, idade, cidade, estado)
 
-  return List;
-}();
+/* VALORES PADRÃO */
 
-var TodoList = /*#__PURE__*/function (_List) {
-  _inherits(TodoList, _List);
+/*const soma = (a = 3, b = 6) => a + b
+console.log(soma(2))*/
 
-  var _super = _createSuper(TodoList);
+/* Arrow Function */
+//const arr = [1, 3, 4, 5, 6]
 
-  function TodoList() {
-    var _this;
+/*
+const newArr = arr.map(item => {
+    return item * 2;
+})*/
+// retornando a um array
+//const teste = () => ['Maça', 'Melão', 'Uva']
+// retornando a um objeto (colocar chaves entre parênteses)
+//const teste = () => ({ nome: 'Edson Carlos' })
+//linha abaixo equivale a de cima
+//const newArr = arr.map(item => item * 2)
+//console.log(teste())
 
-    _classCallCheck(this, TodoList);
+/*
+const arr = [1, 3, 4, 5, 8, 9]
 
-    _this = _super.call(this);
-    _this.usuario = 'Edson';
-    return _this;
-  }
+const newArr = arr.map(function (item, index) {
+    return item + index;
+})
 
-  _createClass(TodoList, [{
-    key: "mostraUsuario",
-    value: function mostraUsuario() {
-      console.log(this.usuario);
+console.log(newArr)
+
+const sum = arr.reduce(function (total, next) {
+    return total + next
+})
+
+console.log(sum)
+
+const filter = arr.filter(function (item) {
+    return item % 2 === 0
+})
+
+console.log(filter)
+
+const find = arr.find(function (item) {
+    return item === 4
+})
+
+console.log(find)*/
+
+/* COM HERANÇA
+class List {
+    constructor() {
+        this.data = []
     }
-  }]);
 
-  return TodoList;
-}(List);
+    add(data) {
+        this.data.push(data)
+        console.log(this.data)
+    }
+}
 
-var MinhaLista = new TodoList();
+class TodoList extends List {
+    constructor() {
+        super()
+
+        this.usuario = 'Edson'
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario)
+    }
+}
+
+const MinhaLista = new TodoList()
 
 document.getElementById('novotodo').onclick = function () {
-  MinhaLista.add('Novo Todo');
-};
+    MinhaLista.add('Novo Todo')
+}
 
-MinhaLista.mostraUsuario();
+MinhaLista.mostraUsuario()
+*/
+
 /*class TodoList {
     constructor() {
         this.todos = []
@@ -89,3 +181,4 @@ const MinhaLista = new TodoList();
 document.getElementById('novotodo').onclick = function () {
     MinhaLista.addTodos();
 }*/
+"use strict";
--- main.js
+++ main.js
@@ -1,4 +1,143 @@
-// COM HERANÇA
+// SINTAXE CURTA DE OBJETO
+
+/*
+const nome = "Edson"
+const idade = 38
+
+const usuario = {
+    nome,
+    idade,
+    empresa: 'ENI'
+}
+
+console.log(usuario)*/
+
+// TEMPLATE LITTERS
+
+/*
+const nome = "Edson"
+const idade = 38
+
+console.log(`Meu nome é ${nome} e tenho ${idade}`)*/
+
+// SPREAD
+/*
+const usuario1 = {
+    nome: 'Edson',
+    idade: 38,
+    empresa: 'ENI'
+}
+
+const usuario2 = { ...usuario1, nome: 'Carlos' }
+
+console.log(usuario2)
+*/
+
+/*
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
+
+const arr3 = [...arr1, ...arr2]
+*/
+
+
+// REST
+
+/*
+function soma(a, b, ...params) {
+    return params
+}*/
+
+/*
+function soma(...params) {
+    return params.reduce((total, next) => total + next)
+}*/
+
+//console.log(arr3)
+
+/*
+const arr = [1, 2, 3, 4]
+
+const [a, ...c] = arr
+*/
+/*
+const usuario = {
+    nome: 'Edson',
+    idade: 38,
+    empresa: 'ENI'
+}
+
+const { nome, ...resto } = usuario
+*/
+
+/* DESESTRUTURAÇÃO */
+/*const usuario = {
+    nome: 'Edson',
+    idade: 38,
+    endereco: {
+        cidade: 'Nova Iguaçu',
+        estado: 'RJ',
+    }
+}*/
+
+//const { nome, idade, endereco: { cidade, estado } } = usuario
+
+// TAMBÉM EM FUNÇÃO
+//function mostraNome({ nome, idade }) {
+//console.log(nome, idade)
+//}
+//mostraNome(usuario)
+
+//console.log(nome, idade, cidade, estado)
+/* VALORES PADRÃO */
+/*const soma = (a = 3, b = 6) => a + b
+console.log(soma(2))*/
+
+/* Arrow Function */
+//const arr = [1, 3, 4, 5, 6]
+/*
+const newArr = arr.map(item => {
+    return item * 2;
+})*/
+
+// retornando a um array
+//const teste = () => ['Maça', 'Melão', 'Uva']
+
+// retornando a um objeto (colocar chaves entre parênteses)
+//const teste = () => ({ nome: 'Edson Carlos' })
+//linha abaixo equivale a de cima
+//const newArr = arr.map(item => item * 2)
+
+//console.log(teste())
+
+/*
+const arr = [1, 3, 4, 5, 8, 9]
+
+const newArr = arr.map(function (item, index) {
+    return item + index;
+})
+
+console.log(newArr)
+
+const sum = arr.reduce(function (total, next) {
+    return total + next
+})
+
+console.log(sum)
+
+const filter = arr.filter(function (item) {
+    return item % 2 === 0
+})
+
+console.log(filter)
+
+const find = arr.find(function (item) {
+    return item === 4
+})
+
+console.log(find)*/
+
+/* COM HERANÇA
 class List {
     constructor() {
         this.data = []
@@ -29,7 +168,7 @@ document.getElementById('novotodo').onclick = function () {
 }
 
 MinhaLista.mostraUsuario()
-
+*/
 /*class TodoList {
     constructor() {
         this.todos = []
